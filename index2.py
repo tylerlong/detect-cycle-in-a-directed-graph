3f271ae55860faf29ae96ad930f308fa37a715d8
Tyler Long <tyler4long@gmail.com>
2022-04-12 16:37:03 -0700

    Add some comments

--- index2.py
+++ index2.py
@@ -2,7 +2,11 @@ n = 5
 pairs1 = [(0, 1), (2, 1), (2, 3), (3, 4), (4, 0)]
 pairs2 = [(0, 1), (2, 1), (2, 3), (3, 4), (4, 0), (4, 2)]
 
-visited = [False] * 5
+# Ref: https://www.youtube.com/watch?v=0dJmTuMrUZM
+# 但是并没有完全参考它的。比如它有 backtracking，我的实现没有，因为我 clone 了 visited。
+# 视频例子后面的实现其实也没有包含backtracking的内容，因为C++是pass by value的。
+# 思路上要把大问题变成多个小问题。比如说要对每个节点作为起点visit一次，那就定义出一个新的function
+
 def visit(pairs, start, visited):
     if visited[start]:
         return True
